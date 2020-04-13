2f0bab4a8c840f7fc39e0a285ac0f56d7b28d246
Tyler Cote <>
2020-04-13 13:42:27 -0400

    Style fix?

--- src/pages/education.js
+++ src/pages/education.js
@@ -2,7 +2,6 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import './education.css';
 import { graphql } from 'gatsby'
 
 const EducationPage = (props) => (
